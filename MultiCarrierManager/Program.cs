3fc9d20bfc42ad8b5214b2ef483e3c84b0b71922
mck <nathanieladventure@gmail.com>
2023-02-18 22:43:09 +0000

    added update checker

--- MultiCarrierManager/Program.cs
+++ MultiCarrierManager/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MultiCarrierManager.CATS;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace MultiCarrierManager {
     static class Program {
@@ -22,6 +24,24 @@ namespace MultiCarrierManager {
             Application.SetCompatibleTextRenderingDefault(false);
             
             logger.Log("Start");
+            
+            // Check for updates by checking the latest github release
+            try {
+                var client = new RestClient("https://api.github.com/repos/");
+                var request = new RestRequest("mck-9061/CATS/releases/latest");
+                var response = client.Execute(request);
+                dynamic json = JsonConvert.DeserializeObject(response.Content);
+                string tag = json.tag_name;
+                
+                if (tag != "1.1.1") {
+                    MessageBox.Show("Update available: " + tag + ". Please download the latest version from GitHub.");
+                }
+            } catch (Exception e) {
+                logger.Log("Error checking for updates: " + e.Message);
+            }
+
+
+
 
             if (settings.OpenToTraversal) {
                 Application.Run(new CATSForm());
