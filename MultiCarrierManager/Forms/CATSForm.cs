67b490750c6efb0fafb2b7e12d3b2b7b7bd40421
Max Haberer <mhaberer25@gmail.com>
2025-02-05 20:53:08 -0600

    Refactor journal dir handling to allow for user profile expansion
    This QoL change should eliminate the need for the user to manually input the profile name for their journal directory. The "?user_dir?" string will be replaced with the path to the user's profile, and the default installation path will handle the rest. The user should not have to engage with this at all.

--- MultiCarrierManager/Forms/CATSForm.cs
+++ MultiCarrierManager/Forms/CATSForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -7,16 +7,20 @@ using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 using MultiCarrierManager.CATS;
 
-namespace MultiCarrierManager {
-    public partial class CATSForm : Form {
+namespace MultiCarrierManager
+{
+    public partial class CATSForm : Form
+    {
         private CatSitter cats;
-        public CATSForm() {
+        public CATSForm()
+        {
             InitializeComponent();
             this.Shown += new System.EventHandler(this.CATSForm_Shown);
             this.Closed += new System.EventHandler(this.CATSForm_Closed);
         }
 
-        private void CATSForm_Shown(object sender, EventArgs e) {
+        private void CATSForm_Shown(object sender, EventArgs e)
+        {
             cats = new CatSitter(textBox1, this, label1, label4);
             loadFromRouteFile();
             loadSettingsFile();
@@ -25,48 +29,59 @@ namespace MultiCarrierManager {
         }
 
 
-        private void CATSForm_Closed(object sender, EventArgs e) {
+        private void CATSForm_Closed(object sender, EventArgs e)
+        {
             cats.close();
         }
 
-        private void label6_Click(object sender, EventArgs e) {
-            
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
 
-        private void label7_Click(object sender, EventArgs e) {
-            
+        private void label7_Click(object sender, EventArgs e)
+        {
+
         }
 
-        private void populateComboBox() {
+        private void populateComboBox()
+        {
             ComboBox box = comboBox1;
             box.Items.Clear();
 
             box.Items.Add("Before first system in route");
-            foreach (string line in textBox2.Text.Split(Environment.NewLine.ToCharArray())) {
+            foreach (string line in textBox2.Text.Split(Environment.NewLine.ToCharArray()))
+            {
                 if (line == "" || line == Environment.NewLine) continue;
                 box.Items.Add(line);
             }
 
             box.SelectedIndex = 0;
 
-            if (File.Exists("CATS\\save.txt")) {
-                try {
+            if (File.Exists("CATS\\save.txt"))
+            {
+                try
+                {
                     int index = Convert.ToInt32(File.ReadAllText("CATS\\save.txt"));
                     if (index == 0) File.Delete("CATS\\save.txt");
                     else box.SelectedIndex = index;
-                } catch (Exception) {
+                }
+                catch (Exception)
+                {
                     File.Delete("CATS\\save.txt");
                     box.SelectedIndex = 0;
                 }
             }
         }
 
-        private void runButton_Click(object sender, EventArgs e) {
-            if (!cats.isRunning) {
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            if (!cats.isRunning)
+            {
                 Program.logger.Log("TraversalRunning");
-                cats.finalSystem = (string) comboBox1.Items[comboBox1.Items.Count - 1];
-                Program.logger.Log("Destination:"+cats.finalSystem);
-                
+                cats.finalSystem = (string)comboBox1.Items[comboBox1.Items.Count - 1];
+                Program.logger.Log("Destination:" + cats.finalSystem);
+
                 cats.isRunning = true;
                 stopButton.Enabled = true;
                 runButton.Enabled = false;
@@ -78,8 +93,10 @@ namespace MultiCarrierManager {
             }
         }
 
-        private void stopButton_Click(object sender, EventArgs e) {
-            if (cats.isRunning) {
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (cats.isRunning)
+            {
                 Program.logger.Log("TraversalStopped");
                 cats.isRunning = false;
                 stopButton.Enabled = false;
@@ -94,41 +111,58 @@ namespace MultiCarrierManager {
             }
         }
 
-        private void loadFromRouteFile() {
+        private void loadFromRouteFile()
+        {
             TextBox box = textBox2;
             textBox2.Text = "";
             string[] lines = File.ReadAllLines("CATS\\route.txt");
 
-            foreach (string l in lines) {
+            foreach (string l in lines)
+            {
                 box.Text = box.Text + l + Environment.NewLine;
             }
-            
+
             Program.logger.Log("RouteLoaded");
         }
 
         private string webhook;
 
-        private void loadSettingsFile() {
+        private void loadSettingsFile()
+        {
             string[] lines = File.ReadAllLines("CATS\\settings.txt");
             textBox3.Text = "";
             textBox5.Text = "";
-            
-            foreach (string line in lines) {
-                if (line.StartsWith("webhook_url")) {
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("webhook_url"))
+                {
                     webhook = line.Replace("webhook_url=", "");
-                } else if (line.StartsWith("journal_directory")) {
+                }
+                else if (line.StartsWith("journal_directory"))
+                {
                     textBox5.Text = line.Replace("journal_directory=", "");
-                } else if (line.StartsWith("tritium_slot")) {
+                    if (textBox5.Text.Contains("?user_dir?"))
+                    {
+                        // Use question marks because they are invalid in Windows file paths, making it impossible for someone to have a profile that matches this string organically
+                        textBox5.Text = textBox5.Text.Replace("?user_dir?", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+                        Program.logger.Log("?user_dir? placeholder filled with " + Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+                    }
+                }
+                else if (line.StartsWith("tritium_slot"))
+                {
                     textBox3.Text = line.Replace("tritium_slot=", "");
                 }
             }
-            
+
             Program.logger.Log("TraversalSettingsLoaded");
         }
 
-        private void loadButton_Click(object sender, EventArgs e) {
+        private void loadButton_Click(object sender, EventArgs e)
+        {
             OpenFileDialog dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() == DialogResult.OK) {
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
                 try
                 {
                     var filePath = dialog.FileName;
@@ -145,34 +179,40 @@ namespace MultiCarrierManager {
         }
 
         // Save settings
-        private void button1_Click(object sender, EventArgs e) {
+        private void button1_Click(object sender, EventArgs e)
+        {
             string journal = textBox5.Text;
             string slot = textBox3.Text;
             string route = textBox2.Text;
             int index = comboBox1.SelectedIndex;
-            
+
             string[] oldLines = File.ReadAllLines("CATS\\settings.txt");
-            foreach (string line in oldLines) {
-                if (line.StartsWith("webhook_url")) {
+            foreach (string line in oldLines)
+            {
+                if (line.StartsWith("webhook_url"))
+                {
                     webhook = line.Replace("webhook_url=", "");
                 }
             }
-            
-            string[] lines = new[] { "webhook_url="+webhook, "journal_directory="+journal, "tritium_slot="+slot, "route_file=route.txt" };
+
+            string[] lines = new[] { "webhook_url=" + webhook, "journal_directory=" + journal, "tritium_slot=" + slot, "route_file=route.txt" };
             File.WriteAllLines("CATS\\settings.txt", lines);
             File.WriteAllText("CATS\\route.txt", route);
             File.WriteAllText("CATS\\save.txt", Convert.ToString(index));
-            
+
             populateComboBox();
 
             MessageBox.Show("Saved settings and route!");
         }
 
-        private void importButton_Click(object sender, EventArgs e) {
-            OpenFileDialog dialog = new OpenFileDialog() {
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
                 Filter = "CSV Files (*.csv)|*.csv"
             };
-            if (dialog.ShowDialog() == DialogResult.OK) {
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
                 try
                 {
                     var filePath = dialog.FileName;
@@ -180,16 +220,17 @@ namespace MultiCarrierManager {
                     List<string> systems = new List<string>();
 
                     int i = 0;
-                    
-                    foreach (string line in lines) {
+
+                    foreach (string line in lines)
+                    {
                         i++;
                         if (i < 3) continue;
                         systems.Add(line.Split('"')[1]);
                     }
-                    
-                    
+
+
                     File.WriteAllLines("CATS\\route.txt", systems.ToArray());
-                    
+
                     loadFromRouteFile();
                 }
                 catch (SecurityException ex)
@@ -200,8 +241,10 @@ namespace MultiCarrierManager {
             }
         }
 
-        private void button2_Click(object sender, EventArgs e) {
-            if (!SpanshRouteForm.isOpen) {
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!SpanshRouteForm.isOpen)
+            {
                 Hide();
                 SpanshRouteForm.isOpen = true;
                 SpanshRouteForm form = new SpanshRouteForm();
@@ -210,35 +253,42 @@ namespace MultiCarrierManager {
             }
         }
 
-        private void button3_Click(object sender, EventArgs e) {
+        private void button3_Click(object sender, EventArgs e)
+        {
             Hide();
             Form1 form = new Form1();
             form.ShowDialog();
             Close();
         }
 
-        private void CATSForm_FormClosing(object sender, FormClosingEventArgs e) {
-            if (e.CloseReason == CloseReason.UserClosing) {
+        private void CATSForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
                 Program.logger.Log("End");
                 Program.logger.Upload();
             }
         }
 
-        private void OptionsButton_Click(object sender, EventArgs e) {
+        private void OptionsButton_Click(object sender, EventArgs e)
+        {
             OptionsForm form = new OptionsForm();
             form.ShowDialog();
         }
 
-        private void button4_Click(object sender, EventArgs e) {
+        private void button4_Click(object sender, EventArgs e)
+        {
             AboutForm form = new AboutForm();
             form.ShowDialog();
         }
 
-        private void CATSForm_Load(object sender, EventArgs e) {
-            
+        private void CATSForm_Load(object sender, EventArgs e)
+        {
+
         }
 
-        private void button5_Click(object sender, EventArgs e) {
+        private void button5_Click(object sender, EventArgs e)
+        {
             DiscordForm form = new DiscordForm();
             form.ShowDialog();
         }
