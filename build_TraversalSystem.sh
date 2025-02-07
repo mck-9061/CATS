69c03b229edeb8c6af9746999dc2deb83718aa73
Max Haberer <mhaberer25@gmail.com>
2025-02-07 13:22:45 -0600

    Add overwrite old output argument to autopilot build script

--- build_TraversalSystem.sh
+++ build_TraversalSystem.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # If you're on Windows, just copy this command and run it in your terminal. Make sure you have everything installed in your venv
-pyinstaller TraversalSystem/main.py --onedir --name TraversalSystem --distpath TraversalSystem/pyinstaller --workpath TraversalSystem/pyinstaller/build --specpath TraversalSystem/pyinstaller --python-option u
+pyinstaller TraversalSystem/main.py --onedir --name TraversalSystem --distpath TraversalSystem/pyinstaller --workpath TraversalSystem/pyinstaller/build --specpath TraversalSystem/pyinstaller --python-option u --noconfirm
