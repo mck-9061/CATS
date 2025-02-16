dcee0b1a265a805e48cc1bd43e637c98cf916925
mck-9061 <32172648+mck-9061@users.noreply.github.com>
2025-02-16 14:24:23 +0000

    Remove Discord links

--- MultiCarrierManager/Forms/AboutForm.Designer.cs
+++ MultiCarrierManager/Forms/AboutForm.Designer.cs
@@ -45,7 +45,7 @@ namespace MultiCarrierManager {
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(190, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Join Discord";
+            this.button1.Text = "Issues on GitHub";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
